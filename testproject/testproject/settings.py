1e1c207e4987e90fc842f751288936471e57e6bd
apurva <apurvavyas96@gmail.com>
2022-06-22 09:21:41 -0700

    Templates Directory Setting Added.

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'testproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['testproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
